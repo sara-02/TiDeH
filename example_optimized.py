e70ea88df9f9befcca5a999576cb8efb6baf7612
sara-02 <sarahmasud02@gmail.com>
2019-10-07 17:55:19 +0530

    Add flush, args and json

--- example_optimized.py
+++ example_optimized.py
@@ -30,10 +30,30 @@ This code is developed by Sylvain Gauthier and Sebastian Rühl under the supervi
 from tideh import load_events_vec
 from tideh import estimate_parameters_optimized
 from tideh import predict_optimized
+import argsparse
+import os
+import json
+import sys
 
-filename = 'data/example/sample_file.txt'
-obs_time = 48  # observation time of 2 days
-pred_time = 168  # predict for one week
+input_path = os.path.join("data","reddit_data","OCT_INPUT")
+output_path = os.path.join("data","reddit_data","oCT_OUTPUT")
+
+parser = argparse.ArgumentParser(description='Input Params')
+parser.add_argument("--srd", help='subreddit_number')
+parser.add_argument('--fl', help='file_number')
+parser.add_argument('--ot', help='observation time in hrs')
+parser.add_argument('--pt', help='prediction time in hrs')
+
+args = parser.parse_args()
+subreddit_number = args.srd
+filename = args.fl + ".txt"
+filename = os.path.join(input_path, subreddit_number, args.fl)
+obs_time = int(args.ot) if args.ot else 24
+pred_time = int(args.pt) if args.pt else 744 # 24 * 31
+
+# filename = 'data/example/sample_file.txt'
+# obs_time = 48  # observation time of 2 days
+# pred_time = 168  # predict for one week
 
 # the number of retweets is not necessary for the further steps
 # make sure that all times are loaded in the correct time unit (hours)
@@ -45,18 +65,45 @@ add_params = {'t0': start_time, 'bounds': [(-1, 0.5), (1, 20.)]}
 
 params, err, _ = estimate_parameters_optimized(event_times=event_times, follower=follower, obs_time=obs_time,
                                                **add_params)
+results = {}
+results['p0'] = params[0]
+results['r0'] = params[1]
+results['phi0'] = params[2]
+results['tm'] = params[3]
+results['avg_fit_error'] = err * 100
 
 print("Estimated parameters are:")
+sys.stdout.flush()
 print("p0:   %.3f" % params[0])
+sys.stdout.flush()
 print("r0:   %.3f" % params[1])
+sys.stdout.flush()
 print("phi0: %.3f" % params[2])
+sys.stdout.flush()
 print("tm:   %.3f" % params[3])
+sys.stdout.flush()
 print("Average %% error (estimated to fitted): %.2f" % (err * 100))
+sys.stdout.flush()
 
 # predict future retweets
 _, total, pred_error = predict_optimized(event_times=event_times, follower=follower, obs_time=obs_time,
                                          pred_time=pred_time, p_max=None, params=params, **add_params)
 
+
+results['from_to_pred'] = total
+total_pred = len([e for e, _ in events if e <= obs_time]) + total
+results['total_pred_at'] = total_pred
+results["pred_abs_error"] = pred_error
+
 print("Predicted number of retwests from %s to %s hours: %i" % (obs_time, pred_time, total))
-print("Predicted number of retweets at hour %s: %i" % (pred_time, event_times[event_times <= obs_time].size + total))
+sys.stdout.flush()
+print("Predicted number of retweets at hour %s: %i" % total_pred)
+sys.stdout.flush()
 print("Prediction error (absolute): %.0f" % pred_error)
+sys.stdout.flush()
+
+filename = "RD_" + args.fl + ".json"
+output_file = os.path.join(output_path, subreddit_number, filename)
+with open(output_file,"w") as f:
+  json.dump(result, f)
+print("End")
