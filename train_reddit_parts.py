d17ed7341f72ea5c007efaa6e7652300c912de87
sara-02 <sarahmasud02@gmail.com>
2019-10-08 12:03:52 +0530

    Fix minor typo in str+

--- train_reddit_parts.py
+++ train_reddit_parts.py
@@ -1,9 +1,10 @@
 import json
 import os
 import subprocess
+import sys
 
-# main_sub_reddit_list = ['8', '24', '27', '20', '33', '35', '22', '26', '36', '10', '34', '23', '28', '21', '9', '15', '30', '16', '19', '17', '31', '6', '29', '7', '32', '18', '37', '25']
-sub_reddit_list = ['6']
+
+sub_reddit_list = ['10', '9']
 
 main_dir = os.path.join("data", "reddit_data")
 
@@ -12,14 +13,18 @@ error_list = []
 
 try:
     for sub_red in sub_reddit_list:
+        print("######################################")
+        print("######################################")
         print(sub_red)
-        str_name = sub_red +"_"
+        sys.stdout.flush()
+        str_name += sub_red +"_"
         for _, _, f in os.walk(
                 os.path.join("data", "reddit_data", "OCT_INPUT", sub_red)):
             file_list = f
         file_list = [f.split(".txt")[0] for f in file_list]
         for each_post in file_list:
             print(each_post)
+            sys.stdout.flush()
             cmd = "python3 example_optimized.py --srd {0} --fl {1}".format(
                 sub_red, each_post)
             returned = subprocess.call(cmd, shell=True)
